a2a517724077d13d417527794ad4a46db263cce8
Mahima <mahima@pushowl.com>
2021-10-06 17:43:58 +0530

    Environment variable fixed

--- simple_push_notification/settings.py
+++ simple_push_notification/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'simple_push_notification.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': os.environ['DATABASE_HOST'],
+        'NAME': os.environ.get("DATABASE_NAME"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': os.environ.get("DATABASE_HOST"),
         'PORT': 5432,
     }
 }
