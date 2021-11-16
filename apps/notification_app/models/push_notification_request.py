ccf623fb30213d74f57544a7fc48422e917a10b8
Mahima De <mahima@pushowl.com>
2021-11-16 16:11:37 +0530

    Removed str function from model files

--- apps/notification_app/models/push_notification_request.py
+++ apps/notification_app/models/push_notification_request.py
@@ -8,6 +8,3 @@ class PushNotificationRequest(models.Model):
     status = models.CharField(max_length=32, choices=PushNotificationRequestStatus.choices,
                               default=PushNotificationRequestStatus.IN_PROGRESS)
     notification = models.ForeignKey(Notification, null=True, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return f'{self.id} {self.status} {self.notification_id}'
